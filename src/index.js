602f6b3e63549bbf424858fc3e3b97230584138f
Neil5838 <neilSE5838@gmail.com>
2025-01-18 13:13:00 +0545

    final commit all done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById('input-box');
 const listDiv = document.getElementById('task-list');
 const addBtn = document.getElementById('addBtn');
+const msg = document.getElementById('msg');
 
 addBtn.addEventListener('click', function() {
     const task = inputBox.value.trim();
@@ -21,5 +22,47 @@ addBtn.addEventListener('click', function() {
     `;
 
     listDiv.appendChild(li);
+    msg.style.display = 'none';
 
-})
+    //functionalities
+
+    const checkbox = li.querySelector("input");
+    const taskSpan = li.querySelector("label > span");
+    const delBtn = li.querySelector("#del-btn");
+    const editBtn = li.querySelector("#edit-btn");
+
+    checkbox.addEventListener('click', function() {
+        li.classList.toggle("completed", checkbox.checked);
+        updateCounter();
+
+    })
+
+    editBtn.addEventListener('click', function() {
+        const update = prompt('Edit task:', taskSpan.textContent);
+        if(update !== null) {
+            taskSpan.textContent = update;
+            li.classList.remove("completed");
+            checkbox.checked = false;
+            updateCounter();
+        }
+    });
+
+    delBtn.addEventListener('click', function() {
+        li.remove();
+        updateCounter()
+    })
+
+
+    const completedCounter = document.getElementById('completed-counter');
+    const uncompletedCounter = document.getElementById('uncompleted-counter');
+
+    function updateCounter() {
+        const completedTask = document.querySelectorAll('.completed').length;
+        const uncompletedTask = document.querySelectorAll('li:not(.completed)').length;
+        completedCounter.innerHTML = completedTask;
+        uncompletedCounter.innerHTML = uncompletedTask;
+    }
+
+    updateCounter();
+
+});
